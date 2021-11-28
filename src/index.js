82932a41636db6834e4976b1c8923314c9b0f918
tomoa112 <78849244+tomoa112@users.noreply.github.com>
2021-11-28 22:55:24 +0900

    htmlとcssを修正

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-const myProfile = ["じゃけ", 28];
-const message3 = `私の名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-console.log(message3); // 私の名前はじゃけです。年齢は28歳です。
+import "./styles.css";
 
-//分割代入(配列）配列の値を変数として宣言・利用して
-//         myProfile.name のように配列名を使わずに値を参照できるようになる。
-const [name, age] = myProfile;
-// 配列値の 0 が最初の変数（name）に入り、1 が次の変数（age)に入る
-// かっこが[]に注意！ 変数の順番も大事になる。
-const message4 = `私の名前は${name}です。年齢は${age}歳です。`;
-console.log(message4); // 私の名前はじゃけです。年齢は28歳です。
+// 追加ボタンをクリックされた時の処理を書く
+const onClickAdd = () => {
+  // inputText変数に テキストボックスの値を代入
+  const inputText = document.getElementById("add-text").value;
+  alert(inputText); // まず機能の前にクラスが正しく実行されるかを確認する。
+};
+
+// html で id=add-button のボタンの処理を実装
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
