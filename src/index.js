24acb570a715ace62033eb9e81eb14d2074d1819
tomoa112 <78849244+tomoa112@users.noreply.github.com>
2021-11-28 23:30:34 +0900

    リスト要素をJavaScriptで追加

--- src/index.js
+++ src/index.js
@@ -4,10 +4,29 @@ import "./styles.css";
 const onClickAdd = () => {
   // inputText変数に テキストボックスの値を代入
   const inputText = document.getElementById("add-text").value;
-  alert(inputText); // まず機能の前にクラスが正しく実行されるかを確認する。
+  // 変数に入ったので、 テキストボックスの値を初期化する（消す）
+  document.getElementById("add-text").value = "";
+  alert(inputText); // 値をとれているかボタンを押してアラートを確認
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row"; // divタグにクラス名を追加する   <div class="list-row"></div>
+  console.log(div); // dev 変数に   <div></div> が入る。
+
+  // pタグ生成
+  const p = document.createElement("p");
+  p.innerText = inputText; // テキストボックスの値を代入
+  console.log(p);
+
+  // div タグの子要素に各要素を設定
+  div.appendChild(p);
+  console.log(div);
+
+  // 未完了のリスト li に追加
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
-// html で id=add-button のボタンの処理を実装
+// html で id=add-button のボタンのクリックイベント時にonClickAdd関数を実行
 document
   .getElementById("add-button")
   .addEventListener("click", () => onClickAdd());
